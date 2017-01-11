e9f05d26ab39a42c3009b81288bec5ce37b2edcf
hannah.s <hannah@parabiosis.net>
2017-01-12 02:43:15 +1100

    Fixed some syntax issues and switched to absolute file paths.

--- aws-dyndns.sh
+++ aws-dyndns.sh
@@ -7,7 +7,7 @@ MYHOSTNAME="home.parabiosis.net"
 
 ZONEID=$(aws route53 list-hosted-zones | grep '"Id":' | awk -F "/" '{print $3}' | tr -d '",')
 NEWIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-CURRENTIP=$(dig $MYHOSTNAME +short )
+CURRENTIP=$(dig $MYHOSTNAME +short)
 CHANGETIME=$(date +%Y-%m-%d:%H:%M:%S)
 
 echo "Current DNS record: $CURRENTIP."
@@ -23,25 +23,27 @@ fi
 ## There are other ways to validate an IP. In an ideal world,
 ## I'd be verifying that it's not a private one too.
 
-if [[ ! -z $( ipcalc -c $NEWIP | grep INVALID) ]] ; then
-	echo "No new valid IP address: "$NEWIP". Exiting."
+if [ ! -z $(ipcalc -c $NEWIP | grep INVALID ) ] ; then
+	echo "No new valid IP address: $NEWIP. Exiting."
 exit 1
 fi
 
 
 ## Checking IP and updating DNS, if needed.
 
-if [ $NEWIP != $CURRENTIP ]; then
-	echo "The external IP address has changed to $NEWIP. Updating the A record for $MYHOSTNAME."
-	sed "s/MYHOSTNAME/$MYHOSTNAME/g; s/CHANGETIME/$CHANGETIME/g; s/NEWIP/$NEWIP/g" ~/dynamicdns/template.json > ~/dynamicdns/$MYHOSTNAME.json 
-	aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://$MYHOSTNAME.json 
+if [ $NEWIP != $CURRENTIP ] ; then
+	echo "The external IP address has changed to $NEWIP." 
+	echo "Updating the A record for $MYHOSTNAME."
+	sed "s/MYHOSTNAME/$MYHOSTNAME/g; s/CHANGETIME/$CHANGETIME/g; s/NEWIP/$NEWIP/g" ~/dynamicdns/template.json > /tmp/$MYHOSTNAME.json 
+	aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file:///tmp/$MYHOSTNAME.json 
+		
 		if [ $? = 0 ] ; then
-			echo Done.
+			echo "Done."
 		else
-			echo Fail.
+			echo "Fail."
 		fi
 
-else 
-	echo "The external IP address remains unchanged."
+else
+	echo "The external IP address remains unchanged"
 fi
 
