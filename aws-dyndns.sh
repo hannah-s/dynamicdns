90f23abd35e43a68597c24b510e04aa6717534ed
hannah.s <hannah@parabiosis.net>
2017-01-12 01:51:33 +1100

    Added awscli config file check.

--- aws-dyndns.sh
+++ aws-dyndns.sh
@@ -12,6 +12,13 @@ CHANGETIME=$(date +%Y-%m-%d:%H:%M:%S)
 
 echo "Current DNS record: $CURRENTIP."
 
+
+if [ ! -s ~/.aws/config ] ; then
+	echo "You need to configure awscli first."
+	echo "See http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html for info."
+	exit 1
+fi
+
 ## Making sure we have an actual external IP before proceeding.
 ## There are other ways to validate an IP. In an ideal world,
 ## I'd be verifying that it's not a private one too.
@@ -22,12 +29,17 @@ exit 1
 fi
 
 
-## Checking IP and updating DNS if needed.
+## Checking IP and updating DNS, if needed.
 
 if [ $NEWIP != $CURRENTIP ]; then
-	echo "The external IP address has changed. Updating the A record for $MYHOSTNAME."
-	sed "s/MYHOSTNAME/$MYHOSTNAME/g; s/CHANGETIME/$CHANGETIME/g; s/NEWIP/$NEWIP/g" template.json > $MYHOSTNAME.json 
+	echo "The external IP address has changed to $NEWIP. Updating the A record for $MYHOSTNAME."
+	sed "s/MYHOSTNAME/$MYHOSTNAME/g; s/CHANGETIME/$CHANGETIME/g; s/NEWIP/$NEWIP/g" ~/dynamicdns/template.json > ~/dynamicdns/$MYHOSTNAME.json 
 	aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://$MYHOSTNAME.json 
+		if [ $? = 0 ] ; then
+			echo Done.
+		else
+			echo Fail.
+		fi
 
 else 
 	echo "The external IP address remains unchanged."
